15a058ece42d67f41f5ba041444fee93c925492e
ivanprofpv <akvabanka@gmail.com>
2022-10-21 10:40:29 +0400

    откат миграции и удаление проверки

--- db/migrate/20221020163857_create_answers.rb
+++ db/migrate/20221020163857_create_answers.rb
@@ -2,7 +2,6 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
       t.string :body
-      t.boolean :correct
       t.references :question, foreign_key: true
 
       t.timestamps
