175ae2c5b9ce6ae87dbb2b146d819d40cf73a98c
ivanprofpv <akvabanka@gmail.com>
2022-10-28 16:13:31 +0400

    add sign_out spec

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'User can log out' do
+
+  given(:user) { create(:user) }
+  background { visit new_user_session_path }
+
+  scenario 'Authenticated user tries to log out' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+    click_on 'Log out'
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
