69065fa728eaa93a7820b9909f5e04d6e632b8ac
ivanprofpv <akvabanka@gmail.com>
2022-11-30 22:58:27 +0400

    fix spec

--- spec/features/comments/create_spec.rb
+++ spec/features/comments/create_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'User can add comment for question and answers' do
   given(:user) { create(:user) }
-  given(:question) { create(:question, user: user) }
+  given!(:question) { create(:question, user: user) }
   given!(:answer) { create(:answer, question: question, user: user) }
 
   # describe 'Authenticated user' do
@@ -32,29 +32,33 @@ feature 'User can add comment for question and answers' do
 
   describe 'push to channel' do
     scenario 'Comments appears for another users after create', js: true do
+      Capybara.using_session('user') do
+        sign_in(user)
+        visit question_path(question)
+      end
 
       Capybara.using_session('guest') do
         visit question_path(question)
       end
 
       Capybara.using_session('user') do
-        sign_in(user)
-        visit question_path(question)
-        
-        within '.question-container' do
-          fill_in(id: 'comment_body', with: 'Test question comment')
-          click_button 'Add a comment'
+        within "#question-#{question.id}" do
+          fill_in 'comment:', with: 'question comment'
+          click_on 'Add a comment'
         end
-        save_and_open_page
-        within '.answers' do
-          fill_in(id: 'comment_body', with: 'Test answer comment')
-          click_button 'Add a comment'
+        sleep 2
+        within "#answer-#{answer.id}" do
+          fill_in 'comment:', with: 'answer comment'
+          save_and_open_page
+          click_on 'Add a comment'
         end
+        sleep 2
+        expect(page).to have_content 'question comment'
       end
-
+      sleep 2
       Capybara.using_session('guest') do
-        expect(page).to have_content('Test question comment')
-        expect(page).to have_content('Test answer comment')
+        expect(page).to have_content('question comment')
+        expect(page).to have_content('answer comment')
       end
     end
   end
