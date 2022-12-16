295375ee4119a81412f2f48fc50a91acb511bee8
ivanprofpv <akvabanka@gmail.com>
2022-12-16 10:13:32 +0400

    add profiles list all user and spec for

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::ProfilesController < Api::V1::BaseController
+  def index
+    render json: User.where.not(id: current_resource_owner.id)
+  end
+
   def me
     render json: current_resource_owner
   end
--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -1,26 +1,53 @@
 require 'rails_helper'
 
-describe 'Profile API', type: :request do
+RSpec.describe 'Profile API', type: :request do
   let(:headers) { { "CONTENT_TYPE" => "application/json",
                     "ACCEPT" => "application/json" } }
+
+  describe 'GET /api/v1/profiles' do
+    let(:method) { :get }
+    let(:api_path) { '/api/v1/profiles' }
+
+    it_behaves_like 'API authorizable'
+
+    context 'when authorized' do
+      let(:users) { create_list(:user, 3) }
+      let(:me) { users.first }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+
+      before do
+        get(api_path, params: { access_token: access_token.token }, headers: headers)
+      end
+
+      it 'returns 200' do
+        expect(response).to be_successful
+      end
+
+      it 'returns all users except resource owner' do
+        expect(json['users'].size).to eq 2
+      end
+    end
+  end
 
   describe 'GET /api/v1/profiles/me' do
     let(:method) { :get }
     let(:api_path) { '/api/v1/profiles/me' }
 
+    it_behaves_like 'API authorizable'
+
     context 'authorized' do
       let(:me) { create(:user) }
       let(:access_token) { create(:access_token, resource_owner_id: me.id) }
 
-      before { get api_path, params: { access_token: access_token.token }, headers: headers }
+      before { get '/api/v1/profiles/me', params: { access_token: access_token.token }, headers: headers }
 
-      it 'return status 200' do
+      it 'returns 200 status' do
         expect(response).to be_successful
       end
 
-      it 'return all public fields' do
+      it 'returns all public fields' do
         %w[id email admin created_at updated_at].each do |attr|
-          expect(json[attr]).to eq me.send(attr).as_json
+          expect(json['user'][attr]).to eq me.send(attr).as_json
         end
       end
 
