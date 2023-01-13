15b71450c82216e33b30ef480f789d441d63f3ce
ivanprofpv <akvabanka@gmail.com>
2023-01-13 22:14:07 +0400

    fix bugs

--- app/serializers/answer_serializer.rb
+++ app/serializers/answer_serializer.rb
@@ -1,6 +1,6 @@
-class Api::V1::AnswerSerializer < ActiveModel::Serializer
+class AnswerSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  
+
   attributes :id, :question_id, :body, :created_at, :updated_at, :user_id
 
   belongs_to :user
